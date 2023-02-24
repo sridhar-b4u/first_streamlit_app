ba64b9a0e994826faa6d032bfbcb86e31fa996ba
sridhar-b4u <sridhar.shopmail@gmail.com>
2023-02-24 13:39:46 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,9 @@
-import streamlit
+import streamlit, pandas
 
 streamlit.title('🎂My First StreamLit App')
+streamlit.Header('🎂Display Food List')
+streamlit.text('🎂From Amazon S3')
 
+
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
