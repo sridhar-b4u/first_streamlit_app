2923ac27a33ff37f5d448308406896040396c22c
sridhar-b4u <sridhar.shopmail@gmail.com>
2023-02-27 12:32:15 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,3 +7,5 @@ streamlit.text('🎂From Amazon S3')
 
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 streamlit.dataframe(my_fruit_list)
+
+streamlit.multiselect("Pick Some Fruits by Index:", list(my_fruit_list.index))
