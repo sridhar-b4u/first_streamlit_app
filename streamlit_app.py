ec174ed166216bb18848222090461e1a1099d5bb
sridhar-b4u <sridhar.shopmail@gmail.com>
2023-02-27 12:47:08 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,9 +9,10 @@ my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.co
 streamlit.dataframe(my_fruit_list)
 
 streamlit.multiselect("Pick Some Fruits by Index:", list(my_fruit_list.index))
-#my_fruit_list = my_fruit_list.set_index('Fruit')
-streamlit.multiselect("Pick Some Fruits by Name:", list(my_fruit_list.set_index('Fruit').index))
+#reset index of dataframe to Fruit column
+my_fruit_list = my_fruit_list.set_index('Fruit')
+streamlit.multiselect("Pick Some Fruits by Name:", list(my_fruit_list.index))
 
-streamlit.multiselect("Pick Some Fruits by Name (Choosen some default):", list(my_fruit_list.set_index('Fruit').index),['Avocado', 'Apple'])
+streamlit.multiselect("Pick Some Fruits by Name (Choosen some default):", list(my_fruit_list.index),['Avocado', 'Apple'])
 
-streamlit.multiselect("Pick Some Fruits by Name (Choosen some default gave incorrect):", list(my_fruit_list.set_index('Fruit').index),['Avocado', 'Apples'])
+
