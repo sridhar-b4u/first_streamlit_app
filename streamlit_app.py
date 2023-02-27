cadc519d1a5d6d07d02256df3dce3023df29247e
sridhar-b4u <sridhar.shopmail@gmail.com>
2023-02-27 12:43:51 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,6 +9,7 @@ my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.co
 streamlit.dataframe(my_fruit_list)
 
 streamlit.multiselect("Pick Some Fruits by Index:", list(my_fruit_list.index))
-streamlit.multiselect("Pick Some Fruits by Name:", list(my_fruit_list.set_index('Fruit')))
+#my_fruit_list = my_fruit_list.set_index('Fruit')
+streamlit.multiselect("Pick Some Fruits by Name:", list(my_fruit_list.set_index('Fruit').index))
 
 #streamlit.multiselect("Pick Some Fruits by Name:", list(my_fruit_list.set_index('Fruit')),['Avocado', 'Apple'])
